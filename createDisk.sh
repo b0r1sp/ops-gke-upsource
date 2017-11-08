32b57ea40028239fc4e7bf5feafa820dc0090ccf
Brett Curtis <brett@lzy.sh>
2017-11-08 11:54:25 -0500

    Testing environment changes

--- createDisk.sh
+++ createDisk.sh
@@ -12,7 +12,7 @@ source ${functions}/checks.sh
 checkProgs gcloud
 
 # gcloud work
-gcloud compute disks create --size=50GB upsource-data
-gcloud compute disks create --size=20GB upsource-backups
-gcloud compute disks create --size=20GB upsource-logs
-gcloud compute disks create --size=20GB upsource-conf
+${gcloud} compute disks create --size=50GB upsource-data
+${gcloud} compute disks create --size=20GB upsource-backups
+${gcloud} compute disks create --size=20GB upsource-logs
+${gcloud} compute disks create --size=20GB upsource-conf
