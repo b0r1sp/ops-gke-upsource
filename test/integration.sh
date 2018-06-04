3dafe18e2077b48f9e6eb90cd569bf27f46357ff
Brett Curtis <brett@lzy.sh>
2018-06-04 13:15:59 -0400

    Adding Travis CI & Codecov

---
+++ test/integration.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# set bash-commons
+commons=ops-gcp-kubernetes/ops-bash-commons
+
+# script dependencies
+source ${commons}/functions/colors.sh
+source ${commons}/functions/testing.sh
+source ${commons}/gcloud/functions/setConfig.sh
+source ${commons}/gcloud/functions/debug.sh
+
+# test script setup
+testName=upsource-test
+project=devops-iac-sb
+zone=us-east1-b
+clusterName=${testName}-cluster
+domainName=${testName}.dis.idexx-ops.com
+cleanup=true
+
+# depends
+cmd="setConfig -p ${project} -z ${zone}"
+echoBlue "Running dependency: ${cmd}"
+${cmd} 2>&1
+
+cmd="ops-gcp-kubernetes/gcloud/createK8sCluster.sh -c ${clusterName}"
+echoBlue "Running dependency: ${cmd}"
+${cmd} 2>&1
+
+# test createDisk.sh
+cmd="./createDisk.sh"
+echoCyan "Running test on: ${cmd}"
+${cmd} 2>&1
+results
+
+# test apply.sh
+cmd="ops-gcp-kubernetes/kubectl/apply.sh -d ${domainName}"
+echoCyan "Running test on: ${cmd}"
+${cmd} 2>&1
+results
+
+# cleanup
+if [ "${cleanup}" = true ]; then
+	# gcloud verbosity
+        debug=false
+        gcloudDebug
+        
+        cmd="kubectl delete ns upsource"
+        echoBlue "Running cleanup command: ${cmd}"
+        ${cmd} 2>&1
+
+	# wait for ingress to cleanup
+        sleep 180
+        
+        cmd="${gcloud} container clusters delete ${clusterName}"
+        echoBlue "Running cleanup command: ${cmd}"
+        ${cmd} 2>&1
+        
+        cmd="${gcloud} compute disks delete upsource-data upsource-backups upsource-logs upsource-conf"
+        echoBlue "Running cleanup command: ${cmd}"
+        ${cmd} 2>&1
+fi
+
+# fail overall script if any of the individual results fail
+if [ "${fail}" = true ]; then
+        exit 1
+fi
